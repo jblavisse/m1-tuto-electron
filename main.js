9cc707b2e466b07bc7d25433f2b663e24c8f8e14
Jb <jb@levillageducode.fr>
2023-04-14 09:48:47 +0200

    Create base structure for all OS

--- main.js
+++ main.js
@@ -14,4 +14,17 @@ app.whenReady().then(() => {
   console.log("app is ready");
 
   createWindow();
+
+  // Sur Mac, quand on clique sur l'icône de l'app dans le dock
+  // et que l'app est déjà ouverte, on veut que la fenêtre soit
+  // réaffichée
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Windows et linux
+// Faire en sorte que si toutes les fenêtres de l'app sont fermées, l'app se ferme
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
